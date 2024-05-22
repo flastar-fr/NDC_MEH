d3ffe426b28362985a6ec9d7c4142c6af97c8ff2
flastar-fr <hgbequet@gmail.com>
2024-05-22 14:39:46 +0200

    Initial commit

---
+++ game.py
@@ -0,0 +1,16 @@
+import pyxel
+
+
+class Game:
+    def __init__(self):
+        pyxel.init(256, 256, title="Nuit du Code")
+
+    def update(self):
+        pass
+
+    def draw(self):
+        pass
+
+
+if __name__ == '__main__':
+    game = Game()
